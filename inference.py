30a435b7af2d0923ea4cf7647927bf5d595c2587
Geraldi Dzakwan <geraldi.dzakwan@gmail.com>
2020-12-17 21:13:29 +0700

    Remove all newlines, split doc by full mark to sents; Add debugging logs

--- inference.py
+++ inference.py
@@ -3,8 +3,9 @@ import string
 import argparse
 
 import requests
-# import urllib
-# from urllib.request import urlretrieve
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
+from timeit import default_timer
 
 import  nltk.translate.bleu_score as bleu
 
@@ -16,11 +17,13 @@ parser.add_argument("--target_lang", type=str, help="target language code")
 parser.add_argument("--num_chunks", type=int, help="number of chunks")
 parser.add_argument("--url", type=str, help="service endpoint")
 
-def preprocess_sentence(sentence):
-    sentence = sentence.strip("\n")
-    # sentence = sentence.translate(str.maketrans("", "", string.punctuation))
+START_TIME = default_timer()
+DEBUG = True
 
-    return sentence
+def preprocess_doc_to_sents(doc):
+    doc = doc.translate(str.maketrans("", "", "\n"))
+
+    return doc.split(".")
 
 def compute_average_blue_score(reference_translation, candidate_translations):
     cumulative_blue_score = 0
@@ -32,13 +35,15 @@ def compute_average_blue_score(reference_translation, candidate_translations):
     return cumulative_bleu_score / len(candidate_translations)
 
 def generate_chunks(doc, num_chunks):
+    chunk_size = len(doc) // num_chunks
+
     sentence_chunks = []
     appended_sentence = ""
 
     for i, sentence in enumerate(doc):
         appended_sentence = appended_sentence + " " + sentence
 
-        if (i + 1) % num_chunks == 0:
+        if (i + 1) % chunk_size == 0:
             sentence_chunks.append(appended_sentence)
             appended_sentence = ""
 
@@ -52,7 +57,10 @@ def send_request(url, text, source_lang, target_lang, chunk_num):
         "chunk_num": chunk_num,
     }
 
-    print(payload)
+    if DEBUG:
+        print("REQUEST")
+        print(payload)
+        print("-"*50)
 
     resp = requests.post(url, json=payload)
 
@@ -66,12 +74,68 @@ def run_experiment(source_doc, target_doc, source_lang, target_lang, num_chunks,
         source_chunk = source_doc_chunks[i]
         target_chunk = target_doc_chunks[i]
 
-        print(source_chunk)
-        print(target_chunk)
+        if DEBUG:
+            print("CHUNK NUMBER")
+            print(i)
+            print("-"*50)
+            print("SOURCE CHUNK")
+            print(source_chunk)
+            print("-"*50)
+            print("TARGET CHUNK")
+            print(target_chunk)
+            print("-"*50)
 
         resp = send_request(url, source_chunk, source_lang, target_lang, i)
 
-        print(resp.json())
+        if DEBUG:
+            print("RESPONSE")
+            print(resp.json())
+            print("-"*50)
+
+# def fetch(session, csv):
+#     base_url = "https://people.sc.fsu.edu/~jburkardt/data/csv/"
+#     with session.get(base_url + csv) as response:
+#         data = response.text
+#         if response.status_code != 200:
+#             print("FAILURE::{0}".format(url))
+#
+#         elapsed = default_timer() - START_TIME
+#         time_completed_at = "{:5.2f}s".format(elapsed)
+#         print("{0:<30} {1:>20}".format(csv, time_completed_at))
+#
+#         return data
+#
+# async def get_data_asynchronous():
+#     csvs_to_fetch = [
+#         "ford_escort.csv",
+#         "cities.csv",
+#         "hw_25000.csv",
+#         "mlb_teams_2012.csv",
+#         "nile.csv",
+#         "homes.csv",
+#         "hooke.csv",
+#         "lead_shot.csv",
+#         "news_decline.csv",
+#         "snakes_count_10000.csv",
+#         "trees.csv",
+#         "zillow.csv"
+#     ]
+#     print("{0:<30} {1:>20}".format("File", "Completed at"))
+#     with ThreadPoolExecutor(max_workers=10) as executor:
+#         with requests.Session() as session:
+#             # Set any session parameters here before calling `fetch`
+#             loop = asyncio.get_event_loop()
+#             START_TIME = default_timer()
+#             tasks = [
+#                 loop.run_in_executor(
+#                     executor,
+#                     fetch,
+#                     *(session, csv) # Allows us to pass in multiple arguments to `fetch`
+#                 )
+#                 for csv in csvs_to_fetch
+#             ]
+#             for response in await asyncio.gather(*tasks):
+#                 pass
 
 if __name__ == "__main__":
     args = parser.parse_args()
@@ -91,27 +155,22 @@ if __name__ == "__main__":
 
     for source_file in source_files:
         with open(os.path.join(source_corpus_dir, source_file), "r") as infile:
-            source_sentences = []
-
-            for line in infile:
-                source_sentences.append(preprocess_sentence(line))
-
-            source_docs.append(source_sentences)
+            source_docs.append(preprocess_doc_to_sents(infile.read()))
 
     for target_file in target_files:
         with open(os.path.join(target_corpus_dir, target_file), "r") as infile:
-            target_sentences = []
-
-            for line in infile:
-                target_sentences.append(preprocess_sentence(line))
-
-            target_docs.append(target_sentences)
+            target_docs.append(preprocess_doc_to_sents(infile.read()))
 
     for i in range(0, len(source_docs)):
-        run_experiment(source_docs[i],
+        run_experiment(
+            source_docs[i],
             target_docs[i],
             args.source_lang,
             args.target_lang,
             args.num_chunks,
             args.url,
         )
+
+    # loop = asyncio.get_event_loop()
+    # future = asyncio.ensure_future(get_data_asynchronous())
+    # loop.run_until_complete(future)
